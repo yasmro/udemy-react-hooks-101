ab494b6e6a2dcba0fb13863085408d236fa8e926
Yu Ohno <kantillo0683@gmail.com>
2020-10-03 16:42:22 +0900

    implement: multiple states

--- src/App.js
+++ src/App.js
@@ -1,58 +1,42 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const App = (props) => {
+  // const initialState = {
+  //   name: '',
+  //   price: 1000
+  // };
 
-  const increment = () => {
-    setCount(count + 1)
-  }
+  // const [name, setName] = useState(initialState.name);
+  // const [price, setPrice] = useState(initialState.price);
 
-  const decrement = () => {
-    setCount(count - 1)
-  }
 
-  // 引数を入れる場合
-  const increment2 = () => {
-    setCount(previousCount => previousCount + 1)
-  }
-  const decrement2 = () => {
-    setCount(previousCount => previousCount - 1)
-  }
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
   const reset = () => {
-    setCount(0)
+    setPrice(props.price)
+    setName(props.name)
   }
 
-  const double = () => {
-    setCount(count * 2)
-  }
-
-  const oneThird = () => {
-    // 三の倍数の時だけ3で割る
-    // if(count % 3 === 0){
-    //   setCount(count / 3)
-    // }
-    setCount( previousCount => {
-      if(previousCount % 3 === 0){
-        return previousCount / 3
-      }else{
-        return previousCount
-      }
-    })
-  }
 
   return (
     <div>
-      <h1>Counter: {count}</h1>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
-      <button onClick={increment2}>Increment2</button>
-      <button onClick={decrement2}>Decrement2</button>
-      <button onClick={reset}>Reset</button>
-      <button onClick={double}>Double</button>
-      <button onClick={oneThird}>3の倍数の時だけ3で割る</button>
+      <h1>Multiple State</h1>
+      <p>Name: {name}</p>
+      <p>Price: {price}</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>{props.name}，{props.price}円にReset</button>
+
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
     </div>
   );
 }
+
+// デフォルトでpropsの値を持たせることも
+App.defaultProps = {
+  name: 'Sample',
+  price: 1000
+};
 
 export default App;
