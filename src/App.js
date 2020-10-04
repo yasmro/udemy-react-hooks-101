552b43b592d46c46e5d0144f0443722efa4a84a4
Yu Ohno <kantillo0683@gmail.com>
2020-10-04 17:30:40 +0900

    implement: callback with useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = (props) => {
   const [state, setState] = useState(props);
@@ -8,10 +8,29 @@ const App = (props) => {
   // state.price => priceとするには？
   const {name, price} = state
 
+  // useEffect(関数,
+  // useEffect(() => {f}, )
+  // componentDidUpdateと同じようなもの
+  useEffect(() => {
+    console.log('componentDidUpdate')
+  })
+
+  // componentDidMount -> 第2引数に[]を追加
+  useEffect(() => {
+    console.log('componentDidMount')
+  }, [])
+
+
+  const renderPeriod = () => {
+    console.log('render period')
+    return 'Called renderPeriod'
+  }
+
 
   return (
     <div>
       <h1>Multiple State</h1>
+      {renderPeriod()}
       <p>Name: {name}</p>
       <p>Price: {price}</p>
       <button onClick={() => setState({...state, price: price + 1})}>+1</button>
