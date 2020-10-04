95437690cabcc0624c677c0cd5fb4aa08eb1d884
Yu Ohno <kantillo0683@gmail.com>
2020-10-04 18:28:41 +0900

    implement: reducer for createEvent

---
+++ src/reducers/index.js
@@ -0,0 +1,25 @@
+// action ={
+//     type: 'CREATE_EVENT',
+//     title: 'Urgent - Test',
+//     body: 'Detail'
+// }
+
+const events = (state = [], action) => {
+    // Create Event -> 
+    switch(action.type){
+        case 'CREATE_EVENT':
+            const event = {title: action.title, body: action.body}
+            const length = state.length
+            let id = (length === 0) ? 1 : state[length - 1].id + 1;
+            return [...state, {id, ...event}]
+            
+        case 'DELETE_EVENT':
+            return state
+        case 'DELETE_ALL_EVENTS':
+            return []
+        default:
+            return state
+    }
+}
+
+export default events
